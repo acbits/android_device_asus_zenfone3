0121622418c1edd783db0767104ccf8c396df722
Lipe <lipebs.rr@gmail.com>
2018-09-15 19:02:30 +0000

    RRify
    Change some files to match RR
    
    Change-Id: Id86f9b6d513b103802066a1248e469bdf94d02d5
    Signed-off-by: lipebs <lipebs.rr@gmail.com>

--- rr.mk
+++ rr.mk
@@ -13,14 +13,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, device/asus/zenfone3/full_zenfone3.mk)
+# Inherit RR configs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
-## Device identifier. This must come after all inclusions
+# Include telephony stuff
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from zenfone3 device
+$(call inherit-product, device/asus/zenfone3/device.mk)
+$(call inherit-product, vendor/asus/zenfone3/zenfone3-vendor.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := zenfone3
-PRODUCT_NAME := lineage_zenfone3
+PRODUCT_NAME := rr_zenfone3
 PRODUCT_BRAND := asus
 PRODUCT_MANUFACTURER := asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.name
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.rr.maintainer="lipebs"
